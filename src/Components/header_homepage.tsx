0e94f321620f7023f90788aaca43e9dc5b088da4
Patrik Zehle <pazehle@gmail.com>
2023-01-30 23:07:23 +0100

    Start of header in homepage

---
+++ src/Components/header_homepage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../Styles/header_homepage.css';
+
+const Header_homepage = () => {
+  return(
+      <div className='title' >
+          <h1 className='title1'>
+              Photography
+          </h1>
+          <h1 className='title2' >
+              Portfolio
+          </h1>
+      </div>
+  )
+}
+
+export default Header_homepage;
