aaa40b0e12aab54676211b291a1d8a4453b82cca
Patrik Zehle <pazehle@gmail.com>
2023-04-05 19:18:13 +0200

    Add background

--- src/Components/header_homepage.tsx
+++ src/Components/header_homepage.tsx
@@ -3,15 +3,19 @@ import '../Styles/header_homepage.css';
 
 const Header_homepage = () => {
   return(
+    <div>
+      <img src="/pictures/bg_1.jpeg" className='pic-bg1'/>
       <div className='title' >
-          <h1 className='title1'>
-              Photography
-          </h1>
-          <h1 className='title2' >
-              Portfolio
-          </h1>
+        <h1 className='title1'>
+            Photography
+        </h1>
+        <h1 className='title2' >
+            Portfolio
+        </h1>
       </div>
-     
+      
+
+    </div> 
   )
 }
 
